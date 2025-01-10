5e0bf3b3a8cc45a688006b7b86c882a16de0807f
n2milmin <nicoleamilmine@gmail.com>
2025-01-10 10:23:15 -0500

    Updated App.js to act as nav, added relevant pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home.js";
+import RSVP from "./pages/RSVP";
+import Gallery from "./pages/Gallery.js/index.js";
+import Registry from  "./pages/Registry";
+import Itinerary from "./pages/Itinerary.js/index.js";
+import Acommidations from "./pages/Acommidations.js/index.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/rsvp" element={<RSVP />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/registry" element={<Registry />} /> 
+        <Route path="/itinerary" element={<Itinerary />} /> 
+        <Route path="/acommidations-directions" element={<Acommidations />} />
+      </Routes>
+    </Router>
   );
 }
 
