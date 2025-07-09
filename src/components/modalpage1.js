d14e85f705e649b46c5f365e1eb6241f3ea709b0
n2milmin <nicoleamilmine@gmail.com>
2025-07-09 14:41:24 -0400

    RSVP - moved the form to a component to HOPEFULLY make multiple 'pages' modalpage1 - created

---
+++ src/components/modalpage1.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+
+const Modalpage1 = () => {
+    const [validated, setValidated] = useState(false);
+    const [guests, setGuests] = useState(0);
+
+    const handleAddGuest = () => setGuests(guests + 1);
+    const handleRemoveGuest = () => setGuests(guests - 1);
+
+    const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+    setValidated(true);
+    }
+
+    return (
+        <Form validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="form" as={Row} controlId="form.input">
+                <Form.Label column sm={3}>Your Name</Form.Label>
+                <Form.Control as={Col} type="text"/>
+            </Form.Group>
+            {Array.from(Array(guests)).map((c, index) => {
+                return (
+                <Form.Group className="names" as={Row} controlId={"guest." + {index}}>
+                    <Form.Label column sm={3}>Guest {index + 1}</Form.Label>
+                    <Form.Control as={Col} type="text"/>
+                </Form.Group>
+                )
+            })}
+            { guests < 3 && 
+            <Button onClick={handleAddGuest} variant="secondary">Add Guest</Button> 
+            }
+            { guests > 0 &&
+            <Button onClick={handleRemoveGuest} variant="secondary">Remove Guest</Button>
+            }
+        </Form>
+    )
+}
+
+export default Modalpage1;
--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { Button, Modal, Form, Col, Row } from "react-bootstrap";
+import Modalpage1 from "../components/modalpage1";
 
 const RSVP = () => {
   const [show, setShow] = useState(false);
-  const [validated, setValidated] = useState(false);
-  const [guests, setGuests] = useState(0);
+  // const [validated, setValidated] = useState(false);
+  // const [guests, setGuests] = useState(0);
   
   const handleShow = () => setShow(true);
   const handleClose = () => {
     setShow(false);
-    setGuests(0)
+    // setGuests(0)
   } // Make sure they know data isn't saved
 
   const handleSubmit = (event) => {
@@ -20,9 +21,6 @@ const RSVP = () => {
     }
     setValidated(true);
   }
-
-  const handleAddGuest = () => setGuests(guests + 1);
-  const handleRemoveGuest = () => setGuests(guests - 1);
 
   return (
     <div>
@@ -41,36 +39,7 @@ const RSVP = () => {
         </Modal.Header>
 
         <Modal.Body>
-          <Form validated={validated} onSubmit={handleSubmit}>
-            {/* Way 1 */}
-            <Row>
-              <Col>
-                <Form.Control type="text" placeholder="First name"/>
-              </Col>
-              <Col>
-                <Form.Control type="text" placeholder="Last name"/>
-              </Col>
-            </Row>
-            {/* Way 2 */}
-            <Form.Group className="form" as={Row} controlId="form.input">
-              <Form.Label column sm={3}>Your Name</Form.Label>
-              <Form.Control as={Col} type="text"/>
-            </Form.Group>
-            {Array.from(Array(guests)).map((c, index) => {
-              return (
-                <Form.Group className="names" as={Row} controlId={"guest." + {index}}>
-                  <Form.Label column sm={3}>Guest {index + 1}</Form.Label>
-                  <Form.Control as={Col} type="text"/>
-                </Form.Group>
-              )
-            })}
-              { guests < 3 && 
-                <Button onClick={handleAddGuest} variant="secondary">Add Guest</Button> 
-              }
-              { guests > 0 &&
-                <Button onClick={handleRemoveGuest} variant="secondary">Remove Guest</Button>
-              }
-          </Form>
+          <Modalpage1 />
         </Modal.Body>
         
         <Modal.Footer>
