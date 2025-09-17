2fabfc4cf67ab2dbf206bfb936c80c73d6a2cd99
n2milmin <nicoleamilmine@gmail.com>
2025-09-17 14:31:37 -0400

    RSVP.js - moved the form from modalpg1 to Modal.Body. The cancel and sumbit would've been to complex otherwise - Fixed spacing with classnames

--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -1,17 +1,15 @@
+import "./RSVP.css";
 import { useState } from "react";
-import { Button, Modal, Form, Col, Row } from "react-bootstrap";
-import Modalpage1 from "../components/modalpage1";
+import { Button, Modal, Form, Col, Row, InputGroup } from "react-bootstrap";
 
 const RSVP = () => {
   const [show, setShow] = useState(false);
-  // const [validated, setValidated] = useState(false);
-  // const [guests, setGuests] = useState(0);
-  
-  const handleShow = () => setShow(true);
-  const handleClose = () => {
-    setShow(false);
-    // setGuests(0)
-  } // Make sure they know data isn't saved
+
+  const [validated, setValidated] = useState(false);
+  const [guests, setGuests] = useState(0);
+
+  const handleAddGuest = () => setGuests(guests + 1);
+  const handleRemoveGuest = () => setGuests(guests - 1);
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -19,18 +17,24 @@ const RSVP = () => {
       event.preventDefault();
       event.stopPropagation();
     }
+
     setValidated(true);
-  }
+  };
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+  } // Make sure they know data isn't saved
 
   return (
     <div>
       <h2>RSVP</h2>
 
-      <Button variant="primary" onClick={ handleShow }>Open</Button>
+      <Button variant="primary" onClick={handleShow}>Open</Button>
 
-      <Modal 
+      <Modal
         show={show}
-        onHide={ handleClose }
+        onHide={handleClose}
         backdrop="static"
         keyboard={false}
       >
@@ -39,74 +43,64 @@ const RSVP = () => {
         </Modal.Header>
 
         <Modal.Body>
-          <Modalpage1 />
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            {/* Name */}
+            <Form.Group className="mb-2" controlId="name.input">
+              <InputGroup hasValidation>
+                <Form.Control required type="text" placeholder="Your Name" />
+                <Form.Control.Feedback type="invalid">Name not found.</Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+
+            {/* Additional guests (partners / children) */}
+            {Array.from(Array(guests)).map((c, index) => {
+              return (
+                <Form.Group className="mb-2" controlId={"guest." + { index }}>
+                  <Form.Control required type="text" placeholder="Guest Name"/>
+                  <Form.Control.Feedback type="invalid">Please enter name or remove guest.</Form.Control.Feedback>
+                </Form.Group>
+              )
+            })}
+            <Form.Group className="mb-4 d-flex gap-2">
+              {/* Display add & remove guest buttons */}
+              {guests < 3 &&
+                <Button onClick={handleAddGuest} variant="secondary">Add Guest</Button>
+              }
+              {guests > 0 &&
+                <Button onClick={handleRemoveGuest} variant="secondary">Remove Guest</Button>
+              }
+            </Form.Group>
+
+            {/* Attending radio btn */}
+            <Form.Group className="mb-4" controlId="attendance.radio">
+              <Form.Label>Will you be attending?</Form.Label> <br />
+              <Form.Check inline type="radio" label="Yes" id="yes" />
+              <Form.Check inline type="radio" label="No" id="no" />
+            </Form.Group>
+
+            {/* Meal Preference */}
+
+            {/* Dietary Restrictions */}
+            <Form.Group className="mb-4" controlId="dietary.input">
+              <Form.Label>Are there any dietary restrictions or allergies?</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            {/* Song Request */}
+            <Form.Group className="mb-5" controlId="songrequest.input">
+              <Form.Label>Do you have a song request?</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            <Form.Group className="d-flex justify-content-end gap-2">
+              <Button variant="secondary" onClick={handleClose}>Cancel</Button>
+              <Button type="submit">Submit</Button>
+            </Form.Group>
+          </Form>
         </Modal.Body>
-        
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Submit</Button>
-        </Modal.Footer>
       </Modal>
     </div>
   )
 }
 
 export default RSVP;
-
-/*
-// Toasts for success
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Toast from 'react-bootstrap/Toast';
-
-function DismissibleExample() {
-  const [showA, setShowA] = useState(true);
-  const [showB, setShowB] = useState(true);
-
-  const toggleShowA = () => setShowA(!showA);
-  const toggleShowB = () => setShowB(!showB);
-
-  return (
-    <Row>
-      <Col md={6} className="mb-2">
-        <Button onClick={toggleShowA} className="mb-2">
-          Toggle Toast <strong>with</strong> Animation
-        </Button>
-        <Toast show={showA} onClose={toggleShowA}>
-          <Toast.Header>
-            <img
-              src="holder.js/20x20?text=%20"
-              className="rounded me-2"
-              alt=""
-            />
-            <strong className="me-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
-          </Toast.Header>
-          <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
-        </Toast>
-      </Col>
-      <Col md={6} className="mb-2">
-        <Button onClick={toggleShowB} className="mb-2">
-          Toggle Toast <strong>without</strong> Animation
-        </Button>
-        <Toast onClose={toggleShowB} show={showB} animation={false}>
-          <Toast.Header>
-            <img
-              src="holder.js/20x20?text=%20"
-              className="rounded me-2"
-              alt=""
-            />
-            <strong className="me-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
-          </Toast.Header>
-          <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
-        </Toast>
-      </Col>
-    </Row>
-  );
-}
-
-export default DismissibleExample;
-*/
