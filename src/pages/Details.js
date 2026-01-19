5feeff24325391babb68fa72cf122b88762fe7f3
n2milmin <nicoleamilmine@gmail.com>
2026-01-19 11:36:38 -0500

    Details.js - removed the handles and used to and target in the Link

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,21 +2,9 @@ import './Details.css';
 import { Link } from "react-router-dom";
 
 const Details = () => {
-
-    const handleHotel = () => {
-        // Open new page with Hotel home page or personal booking link? 
-        // const website = "https://www.guestreservations.com/best-western-little-river-inn-simcoe/booking?utm_source=google&utm_medium=cpc&utm_campaign=973005509&gad_source=1&gad_campaignid=973005509&gbraid=0AAAAADiMQMZFf_n_7QfVyYB6wKMbkqWBK&gclid=CjwKCAjwx8nCBhAwEiwA_z__0ydXhOjtLTrHOhkT0SGKkv2oSMMXXzb9BwscVofgRr1-fJ3Sao1P3RoCfTsQAvD_BwE";
-    }
-
-    const handlePhone = () => {
-        // if on mobile, open call option 
-        // const phone = "(833) 311-9043";
-    }
-
-    const handleMaps = () => {
-        // handle sending to google maps 
-        // const hotel_loc = "203 Queens Way West, Simcoe, ON, N3Y 2M9, Canada";
-    }
+    const website = "https://www.guestreservations.com/best-western-little-river-inn-simcoe/booking?utm_source=google&utm_medium=cpc&utm_campaign=973005509&gad_source=1&gad_campaignid=973005509&gbraid=0AAAAADiMQMZFf_n_7QfVyYB6wKMbkqWBK&gclid=CjwKCAjwx8nCBhAwEiwA_z__0ydXhOjtLTrHOhkT0SGKkv2oSMMXXzb9BwscVofgRr1-fJ3Sao1P3RoCfTsQAvD_BwE";
+    const venue = "https://maps.app.goo.gl/jwPjpzKKsGUPkCMz6";
+    const hotel = "https://maps.app.goo.gl/YUXGZZ3sojvkomZ39";
 
     return(
         <div className="details_container">
@@ -54,8 +42,8 @@ const Details = () => {
                     <h2>Accommodations</h2>
                     <p>Best Western Litte River Inn</p> 
                     <p>
-                        <Link onClick={ handleHotel() } style={{color: 'purple'}}>Hotel link</Link>, &nbsp;
-                        <Link onClick={ handlePhone() } style={{color: 'purple'}}>Phone number</Link>
+                        <Link to={website} target='_blank' style={{color: 'purple'}}>Book Online</Link>, &nbsp;
+                        Call: (833) 311-9043
                     </p>
                     <p>Request "The Milmine & Parkins Wedding"</p> 
                     <p>Friday and Saturday nights available</p>
@@ -68,11 +56,11 @@ const Details = () => {
                 <div className='content'>
                     <h2>Directions</h2>
                     <p>Cranberry Creek Gardens</p>
-                    <p><Link onClick={ handleMaps() } style={{color: 'purple'}}>56 Lynedoch Road, Lynedoch ON, N4B 2W4</Link></p>
+                    <p><Link to={venue} target='_blank' style={{color: 'purple'}}>56 Lynedoch Road, Lynedoch ON, N4B 2W4</Link></p>
                     <p>Note: You will be walking on <strong>GRASS</strong></p>
                     <br/>
                     <p>Best Western Little River Inn</p>
-                    <p><Link onClick={ handleMaps() } style={{color: 'purple'}}>203 Queens Way West, Simcoe, ON, N3Y 2M9</Link></p>
+                    <p><Link to={hotel} target='_blank' style={{color: 'purple'}}>203 Queens Way West, Simcoe, ON, N3Y 2M9</Link></p>
                     <p>15 minute drive from the venue</p>
                 </div>
             </div>
